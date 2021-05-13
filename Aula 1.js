ed291c35c39e6923c10ea5fe222532e6aaa6de65
jpsjp <84147633+jpsjp@users.noreply.github.com>
2021-05-13 15:25:12 -0400

    Add files via upload

---
+++ Aula 1.js
@@ -0,0 +1,36 @@
+/*
+    Comentário longo
+*/
+//Declaração
+var variavel;
+
+variavel = "Texto";
+variavel = 2;
+
+console.log("Conteúdo da variável", variavel);
+
+//Condição
+
+if (true){
+    console.log("é verdadeiro");
+} else{
+    console.log("é falso");
+}
+
+var conteudo = (true) ? "Ternário verdadeiro" : "Ternário falso";
+console.log("Valor da variável", conteúdo);
+
+//Repetição
+console.log("Repetição");
+for(i-0; i<10: i+2){
+    console.log("i = ", i);
+}
+
+//Estruturas
+var obj;
+obj = {
+    "Nome" : "João"
+    "Idade" : 28
+}
+
+console.log(obj);
